00380f63b7f6470336709a2abf4363793ca5a5e7
Mikhail Nosaev <72359732+Guf-Hub@users.noreply.github.com>
2023-03-22 01:13:13 +0300

    Update Code.js

--- Code.js
+++ Code.js
@@ -61,12 +61,12 @@ class GenerateImgUrl {
     if (typeof nmId !== "number" || nmId < 0) {
       throw new Error("Invalid nmId value");
     }
-    this.nmId = nmId;
+    this.nmId = parseInt(nmId, 10);
     this.size = photoSize || "c246x328";
     this.number = photoNumber || 1;
   }
 
-  getUrlPart(id) {
+  getHost(id) {
     if (id >= 0 && id <= 143) return "//basket-01.wb.ru";
     if (id >= 144 && id <= 287) return "//basket-02.wb.ru";
     if (id >= 288 && id <= 431) return "//basket-03.wb.ru";
@@ -76,13 +76,15 @@ class GenerateImgUrl {
     if (id >= 1062 && id <= 1115) return "//basket-07.wb.ru";
     if (id >= 1116 && id <= 1169) return "//basket-08.wb.ru";
     if (id >= 1170 && id <= 1313) return "//basket-09.wb.ru";
-    return "//basket-10.wb.ru";
+    if (id >= 1314 && id <= 1601) return "//basket-10.wb.ru";
+    if (id >= 1602 && id <= 1655) return "//basket-11.wb.ru";
+    return "//basket-12.wb.ru";
   }
 
   url() {
-    const vol = Math.floor(this.nmId / 100000);
-    const part = Math.floor(this.nmId / 1000);
-    return `https:${this.getUrlPart(vol)}/vol${vol}/part${part}/${
+    const vol = ~~(this.nmId / 1e5),
+      part = ~~(this.nmId / 1e3);
+    return `https:${this.getHost(vol)}/vol${vol}/part${part}/${
       this.nmId
     }/images/${this.size}/${this.number}.jpg`;
   }
